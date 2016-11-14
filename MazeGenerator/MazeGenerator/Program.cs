7411fa1ba0d3d09b6a4fe1c9251de5a2ae81802b
Yann <merkya@etml.educanet2.ch>
2016-11-14 11:27:20 +0100

    printMaze bases

--- MazeGenerator/MazeGenerator/Program.cs
+++ MazeGenerator/MazeGenerator/Program.cs
@@ -10,6 +10,18 @@ namespace MazeGenerator
     {
         static void Main(string[] args)
         {
+
+            Console.ReadLine();
+        }
+
+        public static void printMaze(bool[] _horizontalDoors, bool[] _verticalDoors)
+        {
+            //horizontal top and bottom lines
+            Console.SetCursorPosition(0, 0);
+            for(int i = 0; i < _horizontalDoors.GetLength(0); i++)
+            {
+                Console.Write("_");
+            }
         }
     }
 }
