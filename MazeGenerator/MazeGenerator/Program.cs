5ad89c957e5ca90778f177341c6c24090f7ee23a
merkya <merkya@etml.educanet2.ch>
2016-11-21 09:32:05 +0100

    break \o/

--- MazeGenerator/MazeGenerator/Program.cs
+++ MazeGenerator/MazeGenerator/Program.cs
@@ -6,22 +6,78 @@ using System.Threading.Tasks;
 
 namespace MazeGenerator
 {
+    // 1 1 1 1
     class Program
     {
+        public/*const x.x*/ static string[] WALL_STRINGS = new string[] { "─", "│", "├", "┤", "┬", "┴", "┌", "┐", "└", "┘", "┼" };
+
+        /// <summary>
+        /// const used to display the maze, minimum value = 2
+        /// </summary>
+        public const int SIZE = 2;
+
+
         static void Main(string[] args)
         {
+            int[,] temp1 = new int[,] { { 15, 15 }, { 15, 15 }, { 15, 15 } };
 
+            printMaze(temp1);
             Console.ReadLine();
         }
 
-        public static void printMaze(bool[] _horizontalDoors, bool[] _verticalDoors)
+        public static void printMaze(int[,] _maze)
         {
-            //horizontal top and bottom lines
             Console.SetCursorPosition(0, 0);
-            for(int i = 0; i < _horizontalDoors.GetLength(0); i++)
+            //go through all the cases
+            for (int x = 0; x < _maze.GetLength(0); x++)
             {
-                Console.Write("_");
+                for (int y = 0; y < _maze.GetLength(1); y++)
+                {
+                    //if the first bit is lit
+                    if ((_maze[x, y] & 1) == 1)
+                    {
+                        //write the top wall
+                        Console.SetCursorPosition(x * SIZE + 1, y * SIZE);
+                        Console.Write(WALL_STRINGS[0]);
+                    }
+
+                    //if the forth bit is lit
+                    if ((_maze[x, y] & 8) == 8)
+                    {
+                        //write the front wall
+                        Console.SetCursorPosition(x  * SIZE , y * SIZE + 1);
+                        Console.Write(WALL_STRINGS[1]);
+                    }
+
+                    //if this is the last case of the row
+                    if(y == _maze.GetLength(1) - 1)
+                    {
+                        //sets the bottom wall if needed
+                        //if the second bit is lit
+                        if ((_maze[x, y] & 2) == 2)
+                        {
+                            //write the bottom wall
+                            Console.SetCursorPosition(x * SIZE + 1, y * SIZE + 2);
+                            Console.Write(WALL_STRINGS[0]);
+                        }
+                    }
+
+                    //if this is the last row
+                    if (x == _maze.GetLength(0) - 1)
+                    {
+                        //sets the right wall if needed
+
+                        //if the third bit is lit
+                        if ((_maze[x, y] & 4) == 4)
+                        {
+                            //write the right wall
+                            Console.SetCursorPosition(x * SIZE + 2, y * SIZE + 1);
+                            Console.Write(WALL_STRINGS[1]);
+                        }
+                    }
+                }
             }
+
         }
     }
 }
