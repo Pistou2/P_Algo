05dcf7c2e6db01a0e6001aaaad2ba4ba7b453398
Yann Merk <merkya@etml.educanet2.ch>
2016-11-28 10:28:16 +0100

    Improved maze grahpics

--- MazeGenerator/MazeGenerator/Program.cs
+++ MazeGenerator/MazeGenerator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,16 +20,23 @@ namespace MazeGenerator
 
         static void Main(string[] args)
         {
-            Maze maze = new Maze(10, 10);
+            while (true)
+            {
+                Maze maze = new Maze(10, 10);
 
-            printMaze(maze.maze);
-            
-            Console.ReadLine();
+                printMaze(maze.maze);
+
+                Console.ReadLine();
+                Console.Clear();
+            }
         }
 
         public static void printMaze(int[,] _maze)
         {
-            #region cases
+            #region cases + case corners
+
+            bool[] tempCornerWalls = new bool[4];
+            string tempCornerText;
             //go through all the cases
             for (int x = 0; x < _maze.GetLength(0); x++)
             {
@@ -45,7 +53,7 @@ namespace MazeGenerator
                     //if the forth bit is lit
                     if ((_maze[x, y] & 8) == 0)
                     {
-                        //write the front wall
+                        //write the left wall
                         Console.SetCursorPosition(x * SIZE, y * SIZE + 1);
                         Console.Write(WALL_STRINGS[1]);
                     }
@@ -76,6 +84,200 @@ namespace MazeGenerator
                             Console.Write(WALL_STRINGS[1]);
                         }
                     }
+
+
+                    //corners
+                    if (x > 0 && y > 0)
+                    {
+
+                        //TODO
+                        Console.SetCursorPosition(x * SIZE, y * SIZE);
+
+
+                        //set the temp bool (Top, bottom, right, left)
+                        tempCornerWalls[0] = ((_maze[x, y - 1] & 8) == 0);
+                        tempCornerWalls[1] = ((_maze[x, y] & 8) == 0);
+                        tempCornerWalls[2] = ((_maze[x, y] & 1) == 0);
+                        tempCornerWalls[3] = ((_maze[x - 1, y] & 1) == 0);
+
+                        #region Char test
+
+                        if (tempCornerWalls[0])
+                        #region Top + ?
+                        {
+                            if (tempCornerWalls[1])
+                            #region Top + Bottom + ?
+                            {
+                                if (tempCornerWalls[2])
+                                #region Top + Bottom + Right + ?
+                                {
+                                    if (tempCornerWalls[3])
+                                    #region Top + Bottom + Right + Left
+                                    {
+                                        tempCornerText = WALL_STRINGS[10];
+                                    }
+                                    #endregion
+                                    else
+                                    #region Top + Bottom + Right
+                                    {
+                                        tempCornerText = WALL_STRINGS[2];
+                                    }
+                                    #endregion
+
+                                }
+                                #endregion
+                                else
+                                #region Top + Bottom + ?
+                                {
+                                    if (tempCornerWalls[3])
+                                    #region Top + Bottom + Left
+                                    {
+                                        tempCornerText = WALL_STRINGS[3];
+                                    }
+                                    #endregion
+                                    else
+                                    #region Top + Bottom
+                                    {
+                                        tempCornerText = WALL_STRINGS[1];
+                                    }
+                                    #endregion
+                                }
+                                #endregion
+                            }
+                            #endregion
+                            else
+                            #region Top + ?
+                            {
+                                if (tempCornerWalls[2])
+                                #region Top + Right + ?
+                                {
+                                    if (tempCornerWalls[3])
+                                    #region Top + Right + Left
+                                    {
+                                        tempCornerText = WALL_STRINGS[5];
+                                    }
+                                    #endregion
+                                    else
+                                    #region Top + Right
+                                    {
+                                        tempCornerText = WALL_STRINGS[8];
+                                    }
+                                    #endregion
+
+                                }
+                                #endregion
+                                else
+                                #region Top + ?
+                                {
+                                    if (tempCornerWalls[3])
+                                    #region Top + Left
+                                    {
+                                        tempCornerText = WALL_STRINGS[9];
+                                    }
+                                    #endregion
+                                    else
+                                    #region Top
+                                    {
+                                        tempCornerText = WALL_STRINGS[1];
+                                    }
+                                    #endregion
+                                }
+                                #endregion
+                            }
+                            #endregion
+                        }
+                        #endregion
+                        else
+                        #region + ?
+                        {
+                            if (tempCornerWalls[1])
+                            #region Bottom + ?
+                            {
+                                if (tempCornerWalls[2])
+                                #region Bottom + Right + ?
+                                {
+                                    if (tempCornerWalls[3])
+                                    #region Bottom + Right + Left
+                                    {
+                                        tempCornerText = WALL_STRINGS[4];
+                                    }
+                                    #endregion
+                                    else
+                                    #region Bottom + Right
+                                    {
+                                        tempCornerText = WALL_STRINGS[6];
+                                    }
+                                    #endregion
+
+                                }
+                                #endregion
+                                else
+                                #region Bottom + ?
+                                {
+                                    if (tempCornerWalls[3])
+                                    #region Bottom + Left
+                                    {
+                                        tempCornerText = WALL_STRINGS[7];
+                                    }
+                                    #endregion
+                                    else
+                                    #region Bottom
+                                    {
+                                        tempCornerText = WALL_STRINGS[1];
+                                    }
+                                    #endregion
+                                }
+                                #endregion
+                            }
+                            #endregion
+                            else
+                            #region + ?
+                            {
+                                if (tempCornerWalls[2])
+                                #region Right + ?
+                                {
+                                    if (tempCornerWalls[3])
+                                    #region Right + Left
+                                    {
+                                        tempCornerText = WALL_STRINGS[0];
+                                    }
+                                    #endregion
+                                    else
+                                    #region Right
+                                    {
+                                        tempCornerText = WALL_STRINGS[0];
+                                    }
+                                    #endregion
+
+                                }
+                                #endregion
+                                else
+                                #region + ?
+                                {
+                                    if (tempCornerWalls[3])
+                                    #region Left
+                                    {
+                                        tempCornerText = WALL_STRINGS[0];
+                                    }
+                                    #endregion
+                                    else
+                                    #region NOTHING ?!?!?!??
+                                    {
+                                        Debug.Fail("LE labyrinthe n'est pas parfait ._.");
+                                        tempCornerText = "";
+                                    }
+                                    #endregion
+                                }
+                                #endregion
+                            }
+                            #endregion
+                        }
+                        #endregion
+                        #endregion
+
+                        Console.Write(tempCornerText);
+                    }
+
                 }
             }
             #endregion
@@ -107,40 +309,74 @@ namespace MazeGenerator
                     if (x == 0 && y > 0 && y % 2 == 0)
                     {
                         Console.SetCursorPosition(x, y);
-                        Console.Write(WALL_STRINGS[2]);
+                        //check if there's a wall at right to write the correct char
+
+                        if ((_maze[x / SIZE, y / SIZE -1] & 2) == 0)
+                        {
+                            Console.Write(WALL_STRINGS[2]);
+                        }
+                        else
+                        {
+                            Console.Write(WALL_STRINGS[1]);
+                        }
                     }
 
                     //right walls
                     else if (x == _maze.GetLength(0) * SIZE - 1 && y > 0 && y % 2 == 0)
                     {
                         Console.SetCursorPosition(x + 1, y);
-                        Console.Write(WALL_STRINGS[3]);
+                        //check if there's a wall at left to write the correct char
+
+                        if ((_maze[x / SIZE, y / SIZE - 1] & 2) == 0)
+                        {
+                            Console.Write(WALL_STRINGS[3]);
+                        }
+                        else
+                        {
+                            Console.Write(WALL_STRINGS[1]);
+                        }
                     }
 
                     //top walls
                     else if (y == 0 && x > 0 && x % 2 == 0)
                     {
                         Console.SetCursorPosition(x, y);
-                        Console.Write(WALL_STRINGS[4]);
+                        //check if there's a wall under to write the correct char
+
+                        if ((_maze[x / SIZE, y / SIZE] & 8) == 0)
+                        {
+                            Console.Write(WALL_STRINGS[4]);
+                        }
+                        else
+                        {
+                            Console.Write(WALL_STRINGS[0]);
+                        }
                     }
 
                     //bottom walls
                     else if (y == _maze.GetLength(1) * SIZE - 1 && x > 0 && x % 2 == 0)
                     {
+
                         Console.SetCursorPosition(x, y + 1);
-                        Console.Write(WALL_STRINGS[5]);
-                    }
-                    //corner
-                    else if (x > 0 && y > 0 && x < _maze.GetLength(0) * SIZE - 1 && y < _maze.GetLength(1) * SIZE - 1 && x % 2 == 0 && y % 2 == 0)
-                    {
-                        Console.SetCursorPosition(x, y);
-                        Console.Write(WALL_STRINGS[10]);
+                        //check if there's a wall on top to write the correct char
+
+                        if ((_maze[x / SIZE, y / SIZE] & 8) ==0 )
+                        {
+                            Console.Write(WALL_STRINGS[5]);
+                        }
+                        else
+                        {
+                            Console.Write(WALL_STRINGS[0]);
+                        }
                     }
                 }
             }
             #endregion
         }
 
+        /// <summary>
+        /// Debug Method
+        /// </summary>
         public static void printMaze(int[,] _maze, int _lastX, int _lastY)
         {
             printMaze(_maze);
